23e285772af2975edc4da47bf57f33a467f328eb
Sergio Lopez Marquez <sergio.lopez@weplan-app.com>
2019-11-14 14:11:01 +0100

    Show support compatibility

--- app/src/main/java/com/cumberland/sample/webview/MainActivity.java
+++ app/src/main/java/com/cumberland/sample/webview/MainActivity.java
@@ -3,13 +3,12 @@ package com.cumberland.sample.webview;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.cumberland.weplansdk.WeplanSdk;
 
